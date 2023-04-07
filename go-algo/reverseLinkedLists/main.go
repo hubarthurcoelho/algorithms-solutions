b76c0602cf18423a0bb3c8545dbdc0f8ee458cf0
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-04-07 01:13:50 -0300

    feat: reversing linked list

---
+++ go-algo/reverseLinkedLists/main.go
@@ -0,0 +1,19 @@
+
+type ListNode struct {
+  Val int
+  Next *ListNode
+}
+ 
+ func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode = nil
+	current := head
+	var next *ListNode = nil
+
+	for current != nil {
+		next = current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	return prev
+}
