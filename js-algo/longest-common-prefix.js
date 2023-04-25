64f8cb4413073906c641e4ae1151853aad485516
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-04-24 22:57:46 -0300

    common prefix

--- js-algo/longest-common-prefix.js
+++ js-algo/longest-common-prefix.js
@@ -1,27 +1,28 @@
 // https://leetcode.com/problems/longest-common-prefix/description/
 
 function longestCommonPrefix(strs) {
-  let smallestWord = 0
-  for (const word of strs) {
-    if (word.length > smallestWord) smallestWord = word.length
-  }
+    let str = ""
+    let start = 0
+    let end = strs[0].length
 
-  let str = ""
- for (let i = 0; i < smallestWord; i += 1) {
-    const currentLetter = strs[0][i]
-    let matchCount = 1;
-    for (let j = 1; j < strs.length; j += 1) {
-      const currentWord = strs[j]
-      if (currentLetter === currentWord[i]) {
-        matchCount += 1
-      }
-    }
-    if (matchCount === strs.length) {
-      str += currentLetter
-    } else {
-      break;
-    }
-  }
+    while (start < end) {
+        const currentLetter = strs[0][start]
+        let match = 1
+        for (let i = 1; i < strs.length; i += 1) {
+            const currentWord = strs[i]
+            if (currentWord[start] === undefined) return str;
+    
+            if (currentLetter === currentWord[start]) {
+                match += 1
+            }
+        }
+        start++
 
-  return str
+        if (match === strs.length) {
+            str += currentLetter
+        } else {
+            break;
+        }
+    }
+    return str
 };
